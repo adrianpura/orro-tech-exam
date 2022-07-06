a826b41fa314c025d318d979dd3868a7bae017b8
adrianpura <pura.adriano@gmail.com>
2022-07-06 16:21:51 +0800

    update functions to convert up to 1billion
    Signed-off-by: adrianpura <pura.adriano@gmail.com>

--- num-to-words.js
+++ num-to-words.js
@@ -1,10 +1,10 @@
-//accepts 0-999 and two decimals 
+//accepts 0 to billions and two decimals 
 //used Math.floor to get the largest integer less than or equal to the number
 //used remainder operator to get the remainder of the given hundred
 //used toString to ensure that the given number is string before using the split method to convert it into an array
+// used .replace fucntion to replace zero on decimals before whole number
 
-
-let ones = ['ZERO', 'ONE', 'TWO', 'THREE', 'FOOR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE'];
+let ones = ['ZERO', 'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE'];
 let teens = ['TEN', 'ELEVEN', 'TWELVE', 'THIRTEEN', 'FOURTEEN', 'FIFTEEN', 'SIXTEEN', 'SEVENTEEN', 'EIGHTEEN', 'NINETEEN'];
 let tens = ['', '', 'TWENTY', 'THIRTY', 'FORTY', 'FIFTY', 'SIXTY', 'SEVENTY', 'EIGHTY', 'NINETY'];
 
@@ -14,29 +14,73 @@ function convert_tens(num) {
     } else if (num >= 10 && num < 20) {
         return teens[num - 10];
     } else {
-        return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+        if (num % 10 === 0) {
+            return tens[Math.floor(num / 10)]
+        } else {
+            return tens[Math.floor(num / 10)] + " " + ones[num % 10];
+        }
+
     }
 }
 
 function convert_hundreds(num) {
     if (num > 99) {
-        return ones[Math.floor(num / 100)] + " HUNDRED AND " + convert_tens(num % 100);
+        if (num % 100 === 0) {
+            return ones[Math.floor(num / 100)] + " HUNDRED "
+        } else {
+            return ones[Math.floor(num / 100)] + " HUNDRED AND " + convert_tens(num % 100);
+        }
     } else {
         return convert_tens(num);
     }
 }
 
+function convert_thousands(num) {
+    if (num >= 1000) {
+        return convert_hundreds(Math.floor(num / 1000)) + " THOUSAND " + convert_hundreds(num % 1000);
+    } else {
+        return convert_hundreds(num);
+    }
+}
+
+function convert_millions(num) {
+    if (num >= 1000000) {
+        return convert_millions(Math.floor(num / 1000000)) + " MILLION " + convert_thousands(num % 1000000);
+    } else {
+        return convert_thousands(num);
+    }
+}
+
+function convert_billions(num) {
+    if (num >= 1000000000) {
+        return (convert_billions(Math.floor(num / 1000000000)) + " Billion " + convert_millions(num % 1000000000));
+    } else {
+        return convert_millions(num);
+    }
+}
+
 function convert_num_with_decimal(num) {
     let nums = num.toString().split('.')
-    let hundreds = convert_hundreds(nums[0])
+    let billions = convert_billions(nums[0])
     if (nums.length == 2) {
-        let decimal = convert_hundreds(nums[1])
-        return hundreds + ' DOLLARS AND ' + decimal + ' CENTS';
+        let decimal = convert_billions(nums[1].replace(/^0+/, ''))
+        if (nums[1] === '00') {
+            return billions + ' DOLLARS '
+        } else {
+            return billions + ' DOLLARS AND ' + decimal + ' CENTS'
+        }
     } else {
-        return hundreds;
+        return billions
     }
 }
 
 
 console.log(convert_num_with_decimal('123.45'))
+console.log(convert_num_with_decimal('100.05'))
+console.log(convert_num_with_decimal('100001.01'))
+console.log(convert_num_with_decimal('1789501.25'))
+console.log(convert_num_with_decimal('789481.00'))
+console.log(convert_num_with_decimal('2156175.50'))
+console.log(convert_num_with_decimal('1111111.11'))
+console.log(convert_num_with_decimal('10002005.77'))
 // ONE HUNDRED AND TWENTY THREE DOLLARS AND FORTY FIVE CENTS
--- num-to-words.php
+++ num-to-words.php
@@ -4,6 +4,8 @@
 <!-- used (string) instead of toString to ensure that the given number is string before using the explode() instead of split method to convert it into an array -->
 <!-- replace string concatenation from + to . -->
 <!-- set the arrays into a  global variable to access inside a function -->
+<!-- used .ltrim fucntion to remove leading zero -->
+
 
 <?php
 $ones = ['ZERO', 'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE'];
@@ -14,13 +16,16 @@ $tens = ['', '', 'TWENTY', 'THIRTY', 'FORTY', 'FIFTY', 'SIXTY', 'SEVENTY', 'EIGH
 function convert_tens($num)
 {
     global $ones, $teens, $tens;
-
     if ($num < 10) {
         return $ones[$num];
     } else if ($num >= 10 && $num < 20) {
         return $teens[$num - 10];
     } else {
-        return $tens[floor($num / 10)] . " " . $ones[$num % 10];
+        if ($num % 10 === 0) {
+            return $tens[floor($num / 10)];
+        } else {
+            return $tens[floor($num / 10)] . " " . $ones[$num % 10];
+        }
     }
 }
 
@@ -29,23 +34,64 @@ function convert_hundreds($num)
     global $ones;
 
     if ($num > 99) {
-        return $ones[floor($num / 100)] . " HUNDRED AND " . convert_tens($num % 100);
+        if ($num % 100 === 0) {
+            return $ones[floor($num / 100)] . " HUNDRED ";
+        } else {
+            return $ones[floor($num / 100)] . " HUNDRED AND " . convert_tens($num % 100);
+        }
     } else {
         return convert_tens($num);
+    }
+}
+
+function convert_thousands($num)
+{
+    if ($num >= 1000) {
+        return convert_hundreds(floor($num / 1000)) . " THOUSAND " . convert_hundreds($num % 1000);
+    } else {
+        return convert_hundreds($num);
+    }
+}
+
+function convert_millions($num)
+{
+    if ($num >= 1000000) {
+        return convert_millions(floor($num / 1000000)) . " MILLION " . convert_thousands($num % 1000000);
+    } else {
+        return convert_thousands($num);
+    }
+}
+
+function convert_billions($num)
+{
+    if ($num >= 1000000000) {
+        return (convert_billions(floor($num / 1000000000)) . " Billion " . convert_millions($num % 1000000000));
+    } else {
+        return convert_millions($num);
     }
 }
 
 function convert_num_with_decimal($num)
 {
     $nums = explode('.', (string)$num);
-    $hundreds = convert_hundreds($nums[0]);
+    $billions = convert_billions($nums[0]);
     if (count($nums) == 2) {
-        $decimal = convert_hundreds($nums[1]);
-        return $hundreds . ' DOLLARS AND ' . $decimal . ' CENTS';
+        if ($nums[1] === "00") {
+            return $billions . ' DOLLARS';
+        } else {
+            $decimal = convert_billions(ltrim($nums[1], "0"));
+            return $billions . ' DOLLARS AND ' . $decimal . ' CENTS';
+        }
     } else {
-        return $hundreds;
+        return $billions . ' DOLLARS';
     }
 }
 
-var_dump(convert_num_with_decimal(123.45));
-// string(57) "ONE HUNDRED AND TWENTY THREE DOLLARS AND FORTY FIVE CENTS"
+var_dump(convert_num_with_decimal('123.45'));
+var_dump(convert_num_with_decimal('100.05'));
+var_dump(convert_num_with_decimal('100001.01'));
+var_dump(convert_num_with_decimal('1789501.25'));
+var_dump(convert_num_with_decimal('789481.00'));
+var_dump(convert_num_with_decimal('2156175.50'));
+var_dump(convert_num_with_decimal('1111111.11'));
+var_dump(convert_num_with_decimal('10002005.77'));
